b48619d186b8d955b65671c58af2c37d2e6a47ed
itlattice <notice@itgz8.com>
2021-09-12 01:06:31 +0800

    修改配置

--- IBoxs.Auth.Demo/apiConfig.cs
+++ IBoxs.Auth.Demo/apiConfig.cs
@@ -11,8 +11,8 @@ namespace IBoxs.Auth.Demo
     /// </summary>
     public class apiConfig
     {
-        public static string AppID = "2021156832275";  //APPID（这里只是作为演示用APPID，随时更换，上线请更换为自己的），查看地址：https://auth.itgz8.com/user/info/appkey.html
+        public static string AppID = "2020910815526";  //APPID（这里只是作为演示用APPID，随时更换，上线请更换为自己的），查看地址：https://auth.itgz8.com/user/info/appkey.html
 
-        public static string AppSecret = "30b9b15da2f00102cdfa067d984ff199bb2ebaa5";  //AppSecret（这里只是作为演示用APPID，随时更换，上线请更换为自己的），查看地址：https://auth.itgz8.com/user/info/appkey.html
+        public static string AppSecret = "088681a695b7140932538073807f1288d2629fca";  //AppSecret（这里只是作为演示用APPID，随时更换，上线请更换为自己的），查看地址：https://auth.itgz8.com/user/info/appkey.html
     }
 }
